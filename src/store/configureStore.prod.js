16dbea49bf4bcc54cddad927586d65bd69705f9f
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2016-09-13 19:45:45 +0200

    Store production setup

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,6 +1,15 @@
-import {createStore} from 'redux';
-import rootReducer from '../reducers';
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
+import {browserHistory} from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
 
-export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+
+
+export default function configureStore(initialState = {animals:[]}) {
+  const routingMiddleware = routerMiddleware(browserHistory);
+  const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(thunk , routingMiddleware)));
+
+  return store;
 }
