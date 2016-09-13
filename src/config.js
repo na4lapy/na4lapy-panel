5392d4ac15a6768f2bbfedce18f12d966ebf9496
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2016-09-13 19:54:56 +0200

    API URL fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export default  process.env.NODE_ENV === 'development' ? 'http://localhost:9000/' : 'https://api.na4lapy.org:9000';
+export default  process.env.NODE_ENV === 'development' ? 'http://localhost:9000/' : 'https://api.na4lapy.org/';
 export const TOKEN_KEY = 'token';
 export const DATE_FORMAT = 'DD MMM, YYYY';
 export const TOAST_DURATION = 2000;
