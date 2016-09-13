6ffd94ce6aa2be42d33eaaef0c34a9046a5bf72c
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2016-09-13 19:47:36 +0200

    Production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -76,7 +76,7 @@ export default {
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-      {test: /\.(jpe?g|png|gif)$/i, loaders: ['file']},
+      {test: /\.(jpe?g|png|gif)$/i, loader: 'url-loader?name=img/[name].[ext]'},
       {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
       {test: /(\.css|\.sass|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']},
       {test: require.resolve('jquery'), loader: 'expose?jQuery!expose?$'}
